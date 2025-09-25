fbf728de8ad2121e6208eca190028dbf5c9ec3ae
Sebastian Kullander <sebastian.kullander@protonmail.com>
2025-09-25 11:39:42 +0200

    frontend: initial commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Stashbox",
+  description: "A personal library for storing inspiration regarding frontend",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+			<h1>font test</h1>
+			<p>font test</p>
+        </div>
+    );
+}
